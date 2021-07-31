7a04a3f4bbc69afe55f601058863d5894c4c703b
JavaHunk2020 <javahunk2020@gmail.com>
2021-07-31 19:02:02 +0530

    compiler code with auth

--- src/main/java/com/clevered/api/controller/PythonCompilerController.java
+++ src/main/java/com/clevered/api/controller/PythonCompilerController.java
@@ -13,10 +13,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.clevered.rest.api.vo.CodeRequest;
 import com.clevered.rest.api.vo.CodeResponse;
 
 /**
@@ -70,13 +72,13 @@ public class PythonCompilerController {
 	 */
 	@PostMapping("/compile")
 	@ResponseBody
-	public synchronized CodeResponse compileCodeOnline(Model model, @RequestParam("code") String pcode) {
-		LOGGER.debug("pcode  [{}] ", pcode);
+	public synchronized CodeResponse compileCodeOnline(@RequestBody CodeRequest codeRequest) {
+		LOGGER.debug("pcode  [{}] ", codeRequest.getCode());
 		// Writing input into input.txt
 		String generatedOutput = "NA";
 		String codeOutput = "The is some problem at server while compiling code!";
 		try {
-			Files.write(Paths.get("Main.py"), pcode.getBytes());
+			Files.write(Paths.get("Main.py"), codeRequest.getCode().getBytes());
 			codeOutput = crunProcess(pythonVersionCommand+" Main.py");
 			if (codeOutput.contains("Main.py")) {
 				codeOutput = codeOutput.substring(codeOutput.indexOf("Main.py"));
---
+++ src/main/java/com/clevered/rest/api/vo/CodeRequest.java
@@ -0,0 +1,15 @@
+package com.clevered.rest.api.vo;
+
+public class CodeRequest {
+	
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+}
